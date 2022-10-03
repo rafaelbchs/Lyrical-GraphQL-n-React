3f0849a706d0ad8bc749a45f7d9c43d3229835d2
rafaelbchs <rafaelbchacins@gmail.com>
2022-10-03 13:11:11 -0500

    added routing

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import {graphql} from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
     
@@ -15,9 +16,18 @@ class SongList extends Component {
     }
     render() {
         if (this.props.data.loading) {return <div>Loading...</div>}
-        return (<ul className="collection">
+        return (
+        <div>
+        <ul className="collection">
             {this.renderSongs()}
-        </ul>)
+        </ul>
+        <Link to="/songs/new"
+        className="btn-floating btn-large red right"
+        >
+            <i className="material-icons">add</i>
+        </Link>
+        </div>
+        )
     }
 }
 
