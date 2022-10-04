b290becc105525c97c90204c025c346d51ac98fd
rafaelbchs <rafaelbchacins@gmail.com>
2022-10-03 19:14:58 -0500

    added delete mutation

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -6,11 +6,17 @@ import query from '../queries/fetchSongs'
 
 class SongList extends Component {
     
+    onSongDelete(id){
+        console.log(id)
+        this.props.mutate({variables: {id}})
+    }
+
     renderSongs(){
-        return (this.props.data.songs.map(song => {
+        return (this.props.data.songs.map(({id, title}) => {
             return (
-                <li key={song.id} className="collection-item">
-                    {song.title}
+                <li key={id} className="collection-item">
+                    {title}
+                    <i className="material-icons" onClick={()=> this.onSongDelete(id)}>delete</i>
                 </li>
             )
         }))
@@ -32,4 +38,14 @@ class SongList extends Component {
     }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+        id
+    }
+}
+`
+
+export default graphql(mutation)(
+    graphql(query)(SongList)
+    );
