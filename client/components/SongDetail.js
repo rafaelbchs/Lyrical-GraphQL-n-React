82eed743416d36f34002d587d14c3c10fa617cd6
rafaelbchs <rafaelbchacins@gmail.com>
2022-10-04 12:16:07 -0500

    added lyric list and mutation boiler plate

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,8 +3,10 @@ import {graphql} from 'react-apollo';
 import {Link} from 'react-router';
 import fetchSong from '../queries/fetchSong';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
+
     render(){
 
         const {song} = this.props.data;
@@ -15,6 +17,7 @@ class SongDetail extends Component {
             <div>
             <Link to="/">Back</Link>
                 <h3>{song.title}</h3>
+                <LyricList lyrics={song.lyrics}/>
                 <LyricCreate songId={this.props.params.id}/>
             </div>
         )
